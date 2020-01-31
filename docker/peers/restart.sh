b140631542a7ddc2d16790df647e033d1b1e8937
shrakie <yiuked@vip.qq.com>
2020-01-31 12:50:56 +0800

    fiexd instaice err

--- docker/peers/restart.sh
+++ docker/peers/restart.sh
@@ -4,4 +4,5 @@ docker-compose down
 rm -rf ../../production/*
 docker-compose up -d
 
-docker logs -f orderer.36sn.com
+#docker logs -f orderer.36sn.com
+docker logs -f peer0.org1.36sn.com
--- docker/peers/scripts/install.sh
+++ docker/peers/scripts/install.sh
@@ -3,8 +3,22 @@
 CHANNEL_NAME="zsjr"
 APP_PATH=/opt/gopath/src/github.com/hyperledger/fabric/peer
 
-# export CORE_PEER_MSPCONFIGPATH=${APP_PATH}/crypto-config/peerOrganizations/org1.36sn.com/users/Admin@org1.36sn.com/msp
+CAFILE=${APP_PATH}/crypto/ordererOrganizations/36sn.com/orderers/orderer.36sn.com/msp/tlscacerts/tlsca.36sn.com-cert.pem
 
-peer chaincode install -n example -v 0.0.1 -p github.com/hyperledger/fabric-demo/chaincode/chaincode_example02/go/
-peer chaincode instantiate -o orderer.36sn.com:7050 -C zsjr -n example -v 0.0.1 -c '{"Args":["init","a","100","b","200"]}' -P 'OR ('\''Org1MSP.member'\'')'
+set -x
+peer chaincode install -n example -v 0.0.1 -p github.com/chaincode/chaincode_example02/go/ --tls --cafile=${CAFILE}
+res=$?
+set +x
+if [ $res -ne 0 ]; then
+    echo "Failed to install chiancode faile..."
+    exit 1
+fi
 
+set -x
+peer chaincode instantiate -o orderer.36sn.com:7050 -C zsjr -n example -v 0.0.1 -c '{"Args":["init","a","100","b","200"]}' -P 'OR ('\''Org1MSP.member'\'')' --tls --cafile=${CAFILE}
+res=$?
+set +x
+if [ $res -ne 0 ]; then
+    echo "Failed to instantiate chiancode faile..."
+    exit 1
+fi
